b2d132655e730a5bedf9e2fbe18ade37cc031864
afrina-miti <afrina270@gmail.com>
2022-05-25 02:10:51 +0600

    First Task

---
+++ Task1/Task1/Controllers/CVController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Task1.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Personal()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult Reference()
+        {
+            return View();
+        }
+      
+    }
+}
